36cd6cee1743f3df7a803ea95260b9629947bebf
caueeugenio <caue_eugenio@hotmail.com>
2024-08-19 21:32:34 -0300

    feat(main): add git workflow

---
+++ src/shared/interceptors/logging.interceptor.ts
@@ -0,0 +1,13 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common'
+import { Observable, tap } from 'rxjs'
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const requqest = context.switchToHttp().getRequest()
+    const now = Date.now()
+    return next
+      .handle()
+      .pipe(tap(() => console.log(`After... ${Date.now() - now}ms`)))
+  }
+}
